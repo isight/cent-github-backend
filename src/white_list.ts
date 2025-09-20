0b38ad7dc5d77a334e2b1bba4afd1469f9f7d565
Glink <49364151+glink25@users.noreply.github.com>
2025-09-20 23:56:24 +0800

    feat: update whitelist

--- src/white_list.ts
+++ src/white_list.ts
@@ -3,6 +3,6 @@ export default [
 	"http://127.0.0.1", // localhost
 	"https://glink25.github.io",
 	"https://oncent.github.io",
-	"https://cent.linkai.work/",
+	"https://cent.linkai.work",
 	"https://cent.link-ai.workers.dev"
 ];
