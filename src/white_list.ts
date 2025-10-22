5f0eb2b2b8fdd022c915986b865830b70818deed
Bingquan Zuo <zuobq@wust.edu.cn>
2025-10-22 13:46:35 +0800

    Update white_list.ts

--- src/white_list.ts
+++ src/white_list.ts
@@ -1,8 +1,5 @@
 export default [
 	"http://localhost", // localhost
 	"http://127.0.0.1", // localhost
-	"https://glink25.github.io",
-	"https://oncent.github.io",
-	"https://cent.linkai.work",
-	"https://cent.link-ai.workers.dev"
+	"https://cent.wust.eu.org"
 ];
