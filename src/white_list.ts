e1e08d243fd253d8daf5ecf6b9cbd9cb29909455
Bingquan Zuo <zuobq@wust.edu.cn>
2025-11-21 18:39:09 +0800

    Update white_list.ts

--- src/white_list.ts
+++ src/white_list.ts
@@ -3,5 +3,6 @@ export default [
 	"http://127.0.0.1", // localhost
 	"http://localhost:5173", // localhost
 	"http://127.0.0.1:5173", // localhost
+	"https://cent-3jp.pages.dev",
 	"https://cent.wust.eu.org",
 ];
